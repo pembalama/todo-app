02d110e0fcaaa7c66fc32da7e797716710711abc
pembalama <pemba.t.lama@gmail.com>
2023-07-05 17:08:59 -0400

    added task input field

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,46 @@
-// Importing React into our component
 import React from 'react';
 
-// Defining and exporting our TodoList component
 export default class TodoList extends React.Component {
-	// The render function describes what the UI will look like for the component
+	constructor(props) {
+		super(props);
+
+		// Initialize state to store the current task input
+		this.state = {
+			currentTask: '',
+		};
+
+		// Bind 'this' to our methods
+		this.updateCurrentTask = this.updateCurrentTask.bind(this);
+		this.addTask = this.addTask.bind(this);
+	}
+
+	updateCurrentTask(event) {
+		// Event handler for input change
+		// Update the currentTask state with the new input
+		this.setState({ currentTask: event.target.value });
+	}
+
+	addTask(event) {
+		// Event handler for form submission
+		event.preventDefault();
+		// For now, just log the current task when the form is submitted
+		console.log(this.state.currentTask);
+	}
+
 	render() {
 		return (
-			// For now, our component will just display a simple message
 			<div>
 				<h1>Welcome to the ToDo List</h1>
+				{/* Form for adding new tasks */}
+				<form onSubmit={this.addTask}>
+					<input
+						type="text"
+						placeholder="Enter new task"
+						value={this.state.currentTask}
+						onChange={this.updateCurrentTask}
+					/>
+					<button type="submit">Add Task</button>
+				</form>
 			</div>
 		);
 	}
